2afcf3cb1d211904392e3f63788c044e1d61a6f4
ehsan_hrz <ehsanheidarzadeh959@gmail.com>
2023-10-20 09:16:26 +0330

    init

---
+++ src/Smartstore.Web/Areas/Admin/Models/Settings/DataExchangeSettingsModel.cs
@@ -0,0 +1,20 @@
+﻿using Smartstore.Core.DataExchange;
+
+namespace Smartstore.Admin.Models
+{
+    [LocalizedDisplay("Admin.Configuration.Settings.DataExchange.")]
+    public partial class DataExchangeSettingsModel
+    {
+        [LocalizedDisplay("*MaxFileNameLength")]
+        public int MaxFileNameLength { get; set; }
+
+        [LocalizedDisplay("*ImageImportFolder")]
+        public string ImageImportFolder { get; set; }
+
+        [LocalizedDisplay("*ImageDownloadTimeout")]
+        public int ImageDownloadTimeout { get; set; }
+
+        [LocalizedDisplay("*ImportCompletionEmail")]
+        public DataExchangeCompletionEmail ImportCompletionEmail { get; set; }
+    }
+}
